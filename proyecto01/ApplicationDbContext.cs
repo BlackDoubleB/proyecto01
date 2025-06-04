813c793df650666df85d2e16b04835131f1a1e59
BlackDoubleB <blancablacido@gmail.com>
2025-06-03 20:45:15 -0500

    Agregar archivos de proyecto.

---
+++ proyecto01/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace proyecto01
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+    }
+}
